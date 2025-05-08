d7436621df3968981db981fe4cb6f5d9b867d2e2
visu <viswanatha.kanda@gmail.com>
2025-05-08 12:22:00 -0500

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-echo "# earlydetect" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/viswanatha1221/earlydetect.git
-git push -u origin main
+1. VPC with public and private subnets in 2 avaialability zones.
+2. An Internet Gateway is used to allow communication between instances in VPC and the internet
+3. we are using 2 Availability Zones for high avaialability and fault tolerance.
+4. Resources such as Nat Gateways, Bastion Host, and Application Load balancer use Public Subnets.
+5. We will put the webservers and database servers in the Private Subents to protect them.
+The Public Route Table is associated with the public subnets and routes traffic to the internet through the internet gateway.
+6. The Main Route Table is associated with the private subnets.
---
+++ main.tf
@@ -0,0 +1,34 @@
+# configure aws provider
+
+provider "aws" {
+    region = var.region
+}
+
+# create vpc
+
+module "vpc" {
+    source                       = "./modules/vpc"
+    region                       = var.region
+    project_name                 = var.project_name
+    vpc_cidr                     = var.vpc_cidr
+    public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+    public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+    private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+    private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+    private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+    private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
+
+# create nat gateway
+
+module "nat_gateway" {
+    source                          = "./modules/nat_gateway"
+    public_subnet_az1_id            = module.vpc.public_subnet_az1_id
+    public_subnet_az2_id            = module.vpc.private_app_subnet_az2_id
+    internet_gateway                = module.vpc.internet_gateway
+    vpc_id                          = module.vpc_id
+    private_app_subnet_az1_id       = module.vpc.private_app_subnet_az1_id
+    private_app_subnet_az2_id       = module.vpc.private_app_subnet_az2_id
+    private_data_subnet_az1_id      = module.vpc.private_data_subnet_az1_id
+    private_data_subnet_az2_id      = module.vpc.private_data_subnet_az2_id
+}
