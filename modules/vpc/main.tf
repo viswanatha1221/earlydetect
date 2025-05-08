83c4208cb624fb0d64bc18de4f6eb209058f3f96
visu <viswanatha.kanda@gmail.com>
2025-05-08 13:03:53 -0500

    formatted the files

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -107,6 +107,7 @@ resource "aws_subnet" "private_data_subnet_az1" {
 
   tags      = {
     Name    = "private data subnet az1"
+ }
 }
 
 # create private data subnet az2
@@ -117,6 +118,6 @@ resource "aws_subnet" "private_data_subnet_az2" {
   map_public_ip_on_launch  = false
 
   tags      = {
-    Name    = private data subnet az2"
+    Name    = "private data subnet az2"
   }
 }
